b775d10c4ea27d0ca5b3f23ebdeca1052688f770
Al4ise <39273158+Al4ise@users.noreply.github.com>
2021-02-14 21:11:28 +0200

    global update command

--- Install.sh
+++ Install.sh
@@ -1,17 +1,25 @@
 #!/bin/zsh
 
 mkdir -p ~/azule/.tmp
-cd $AZULE/.tmp
+cd $AZULE
 git pull
-chmod +x $AZULE/insert_dylib
+chmod +x insert_dylib
+
+# UPDATE-AZULE GLOBAL COMMAND
+if [ ! -f /usr/local/bin/update-azule ]
+then
+ln -s install.sh /usr/local/bin/update-azule
+chmod +x /usr/local/bin/update-azule
+fi
 
 # REMOVE OLD SUBSTRATE
-if [ ! -f $AZULE/CydiaSubstrate.framework ]
+if [ -d CydiaSubstrate.framework ]
 then
-rm -rf $AZULE/CydiaSubstrate.framework
+rm -rf CydiaSubstrate.framework
 fi
 
 # INSTALL SUBSTRATE
+cd .tmp
 curl -s -o api.json 'https://api.parcility.co/db/package/mobilesubstrate/saurik'
 curl -s -Lo substrate.deb "$(jq -r '.data.repo.url' api.json)$(jq -r '.data.builds[-1].Filename' api.json)"
 ar -x substrate.deb data.tar.lzma
@@ -21,5 +29,4 @@ rm -rf "$AZULE/CydiaSubstrate.framework/Libraries" "$AZULE/CydiaSubstrate.framew
 # CLEANUP
 rm -rf $AZULE/.tmp
 
-## UPDATE-AZULE GLOBAL COMMAND
 ## INIT-AZULE GLOBAL COMMAND
