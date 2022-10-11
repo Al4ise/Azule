36c0f12a2d765c4e765ce9424116268a9b9fff63
Al4ise <vapr76@gmail.com>
2022-10-11 18:31:39 +0300

    idk anymore, changes

--- .vscode/build.sh
+++ .vscode/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp azule iOS/usr/bin/azule
-cp modules/azule_apt iOS/usr/lib/Azule
-cp modules/azule_decrypt iOS/usr/lib/Azule
+cp modules/azule_apt iOS/usr/lib/Azule/modules/
+cp modules/azule_decrypt iOS/usr/lib/Azule/modules/
 find iOS -name .DS_Store -exec rm -rf {} \;
 dpkg-deb -b iOS Azule-iOS.deb
