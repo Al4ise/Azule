a991718f8c169dd8bc9e75f26caf51c20be42e6c
Al4ise <vapr76@gmail.com>
2023-02-23 19:31:18 +0200

    fixed some small issues

--- .vscode/build.sh
+++ .vscode/build.sh
@@ -5,3 +5,4 @@ cp modules/azule_apt iOS/usr/lib/Azule/modules/
 cp modules/azule_decrypt iOS/usr/lib/Azule/modules/
 find iOS -name .DS_Store -exec rm -rf {} \;
 dpkg-deb -b iOS Azule-iOS.deb
+rm iOS/usr/bin/azule iOS/usr/lib/Azule/modules/azule_decrypt iOS/usr/lib/Azule/modules/azule_apt 
